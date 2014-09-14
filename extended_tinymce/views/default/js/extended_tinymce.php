6398d9658240e4510c24a234b2936e1bb269e591
iionly <iionly@gmx.de>
2014-09-14 21:02:44 +0200

    Updated to Tinymce editor version 4.1.5

--- extended_tinymce/views/default/js/extended_tinymce.php
+++ extended_tinymce/views/default/js/extended_tinymce.php
@@ -7,18 +7,18 @@ elgg.provide('elgg.extended_tinymce');
  * @return void
  */
 elgg.extended_tinymce.toggleEditor = function(event) {
-    event.preventDefault();
+	event.preventDefault();
 
-    var target = $(this).attr('href');
-    var id = $(target).attr('id');
-    var $link = $(this);
+	var target = $(this).attr('href');
+	var id = $(target).attr('id');
+	var $link = $(this);
 
-    tinyMCE.execCommand('mceToggleEditor', false, id);
-    if ($link.html() == elgg.echo('extended_tinymce:remove')) {
-        $link.html(elgg.echo('extended_tinymce:add'));
-    } else {
-        $link.html(elgg.echo('extended_tinymce:remove'));
-    }
+	tinyMCE.execCommand('mceToggleEditor', false, id);
+	if ($link.html() == elgg.echo('extended_tinymce:remove')) {
+		$link.html(elgg.echo('extended_tinymce:add'));
+	} else {
+		$link.html(elgg.echo('extended_tinymce:remove'));
+	}
 }
 
 /**
@@ -29,33 +29,37 @@ elgg.extended_tinymce.toggleEditor = function(event) {
  */
 elgg.extended_tinymce.init = function() {
 
-    $('.extended_tinymce-toggle-editor').live('click', elgg.extended_tinymce.toggleEditor);
+	$('.extended_tinymce-toggle-editor').live('click', elgg.extended_tinymce.toggleEditor);
 
-    $('.elgg-input-longtext').parents('form').submit(function() {
-        tinyMCE.triggerSave();
-    });
+	$('.elgg-input-longtext').parents('form').submit(function() {
+		tinyMCE.triggerSave();
+	});
 
-    tinyMCE.init({
-        selector: ".elgg-input-longtext",
-        theme: "modern",
-        skin : "lightgray",
-        language : "<?php echo extended_tinymce_get_site_language(); ?>",
-        relative_urls : false,
-        remove_script_host : false,
-        document_base_url : elgg.config.wwwroot,
-        plugins: "advlist autolink autosave charmap code colorpicker emoticons fullscreen hr image insertdatetime link lists paste preview print searchreplace table textcolor textpattern wordcount",
-        menubar: false,
-        toolbar_items_size: "small",
-        toolbar1: "newdocument preview fullscreen print | searchreplace | styleselect | fontselect | fontsizeselect",
-        toolbar2: "undo redo | bullist numlist | outdent indent | bold italic underline | alignleft aligncenter alignright alignjustify | removeformat",
-        toolbar3: "pastetext | insertdatetime | charmap | hr | table | forecolor backcolor | link unlink | image | emoticons | blockquote" + (elgg.is_admin_logged_in() ? " | code" : ""),
-        width : "99%",
-        browser_spellcheck : true,
-        image_advtab: true,
-        paste_data_images: false,
-        insertdate_formats: ["%I:%M:%S %p", "%H:%M:%S", "%Y-%m-%d", "%d.%m.%Y"],
-        content_css: elgg.config.wwwroot + 'mod/extended_tinymce/css/elgg_extended_tinymce.css'
-    });
+	tinyMCE.init({
+		selector: ".elgg-input-longtext",
+		theme: "modern",
+		skin : "lightgray",
+		language : "<?php echo extended_tinymce_get_site_language(); ?>",
+		relative_urls : false,
+		remove_script_host : false,
+		document_base_url : elgg.config.wwwroot,
+		plugins: "advlist autolink autoresize autosave charmap code colorpicker emoticons fullscreen hr image insertdatetime link lists paste preview print searchreplace table textcolor textpattern wordcount",
+		menubar: false,
+		toolbar_items_size: "small",
+		toolbar: [
+			"newdocument preview fullscreen print | searchreplace | styleselect | fontselect | fontsizeselect",
+			"undo redo | bullist numlist | outdent indent | bold italic underline | alignleft aligncenter alignright alignjustify | removeformat",
+			"pastetext | insertdatetime | charmap | hr | table | forecolor backcolor | link unlink | image | emoticons | blockquote" + (elgg.is_admin_logged_in() ? " | code" : "")
+		],
+		width : "99%",
+		browser_spellcheck : true,
+		image_advtab: true,
+		paste_data_images: false,
+		autoresize_min_height: 200,
+		autoresize_max_height: 450,
+		insertdate_formats: ["%I:%M:%S %p", "%H:%M:%S", "%Y-%m-%d", "%d.%m.%Y"],
+		content_css: elgg.config.wwwroot + 'mod/extended_tinymce/css/elgg_extended_tinymce.css'
+	});
 }
 
 elgg.register_hook_handler('init', 'system', elgg.extended_tinymce.init);
